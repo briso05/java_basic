e84106444e4f0b04b74eec5fba33202d5fd465fd
briso05 <sobright05@naver.com>
2021-12-30 22:00:04 +0900

    add Lombok basic project

---
+++ step07_API_Collection_t/src/step02/lombok/Lombok.java
@@ -0,0 +1,17 @@
+package step02.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Lombok {
+	
+	
+	private String name;
+	private int age;
+	
+	
+}
